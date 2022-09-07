84a3759784267b4182cc2a93886cd42c17ab9d97
Sten Uuesoo <sten.uuesoo@gmail.com>
2022-09-07 11:40:17 +0300

    Quotable API integration and MUI component styles.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Button  from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
 import './App.css';
 
 function App() {
-  return (
+
+  const [quote, setQuote] = useState([]);
+
+  useEffect(() => {
+    getQuote()}, []);
+
+    const getQuote = async () => {
+      const response = await fetch('https://api.quotable.io/random');
+      const data = await response.json();
+      console.log(data.content);
+      setQuote(data.content);
+    }
+
+  return (  
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="spacer"></div>
+          <Card variant="outlined" sx={{ maxWidth: 800 }}>
+            <CardContent>
+              <Typography variant="body1" color="text.secondary">
+                {quote}
+              </Typography>
+            </CardContent>
+          </Card>
       </header>
+      <Button variant="contained" onClick={getQuote}>New Quote</Button>
     </div>
-  );
-}
+);}
 
 export default App;
